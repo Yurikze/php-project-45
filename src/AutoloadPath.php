218db2354abde7cbb319806081796330f29912a0
Yuri Zhulev <yuri.zhulyov@gmail.com>
2023-12-22 15:06:43 +0300

    refactor even game

--- src/AutoloadPath.php
+++ src/AutoloadPath.php
@@ -1,7 +1,5 @@
 <?php
 
-// namespace BrainGames\AutoloadPath;
-
 $autoloadPath1 = __DIR__ . '/../../../autoload.php';
 $autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
 if (file_exists($autoloadPath1)) {
--- src/Games/Even.php
+++ src/Games/Even.php
@@ -2,15 +2,29 @@
 
 namespace BrainGames\Games\Even;
 
-function evenGame(): array
-{
-    $randomNumber = rand(1, 100);
-    $isEven = $randomNumber % 2 === 0;
-    $correctAnswer = $isEven ? 'yes' : 'no';
-    return [$correctAnswer, $randomNumber];
-}
-
 function gameRules(): string
 {
     return 'Answer "yes" if the number is even, otherwise answer "no".';
+}
+
+// function evenGame(): array
+// {
+//     $randomNumber = rand(1, 100);
+//     $isEven = $randomNumber % 2 === 0;
+//     $correctAnswer = $isEven ? 'yes' : 'no';
+//     return [$correctAnswer, $randomNumber];
+// }
+
+function gameData(): int
+{
+    return rand(1, 100);
+}
+
+function gameCorrectAnswer(int $randomNumber): string
+{
+    return $randomNumber % 2 === 0 ? 'yes' : 'no';
+}
+
+function gameQuestion(int $randomNumber): string{
+    return "Question: {$randomNumber}";
 }
