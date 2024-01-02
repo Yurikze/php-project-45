6750d4b968cbf50642d6a5a48b548e460b1b08d4
Yuri Zhulev <yuri.zhulyov@gmail.com>
2024-01-02 12:01:59 +0300

    Refactor even game module

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -2,6 +2,10 @@
 
 namespace BrainGames\Games\Even;
 
+use function cli\line;
+use function BrainGames\Cli\greet;
+use function BrainGames\Engine\gameEngine;
+
 function gameRules(): string
 {
     return 'Answer "yes" if the number is even, otherwise answer "no".';
@@ -12,12 +16,34 @@ function gameData(): int
     return rand(1, 100);
 }
 
+function isEven(int $number)
+{
+    return $number % 2 === 0;
+}
+
 function gameCorrectAnswer(int $randomNumber): string
 {
-    return $randomNumber % 2 === 0 ? 'yes' : 'no';
+    return isEven($randomNumber) ? 'yes' : 'no';
 }
 
 function gameQuestion(int $randomNumber): string
 {
     return "Question: {$randomNumber}";
+}
+
+function game()
+{
+    $userName = greet();
+    $gameRules = gameRules();
+    line($gameRules);
+    for ($i = 0; $i < 3; $i++) {
+        $roundData = gameData();
+        $roundQuestion = gameQuestion($roundData);
+        $roundCorrectAnswer = gameCorrectAnswer($roundData);
+        $roundCompleted = gameEngine($userName, $roundQuestion, $roundCorrectAnswer);
+        if (!$roundCompleted) {
+            return;
+        }
+    }
+    line("Congratulations, {$userName}!");
 }
